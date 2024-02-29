9febeb5ecb6a0b4b5a8d261192a9cab0283fb569
“Yuu18id“ <211112007@students.mikroskil.ac.id>
2024-02-29 21:22:00 +0700

    add comments

--- src/Modulo.java
+++ src/Modulo.java
@@ -5,14 +5,26 @@ public class Modulo {
         modulo();
     }
 
+    //Method untuk operasi modulo/sisa bagi
     public static void modulo() {
+        //import scanner untuk sistem input
         Scanner scanner = new Scanner(System.in);
+
+        //tampilan CLI
         System.out.println("Menghitung Modulo (Sisa Bagi)");
         System.out.print("Angka 1: ");
+
+        //variabel untuk menampung angka 1
         int angka1 = scanner.nextInt();
         System.out.print("Angka 2: ");
+
+        //variabel untuk menampung angka 2
         int angka2 = scanner.nextInt();
+
+        //variabel yg menyimpan hasil modulo berdasarkan rumus yg dibuat
         int hasil = angka1 % angka2;
+
+        //cetak hasil akhir
         System.out.println("Hasil Modulo : "+ hasil);
     }
 }
