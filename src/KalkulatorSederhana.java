f873c37dd575886fd08619234de8038540d32768
Renitha <93696670+renithaz@users.noreply.github.com>
2024-03-01 09:48:01 +0700

    add pengurangan tambah komentar

--- src/KalkulatorSederhana.java
+++ src/KalkulatorSederhana.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class KalkulatorSederhana {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        //memasukkan angka pertama
         System.out.print("Masukkan angka pertama: ");
         double angkaPertama = scanner.nextDouble();
 
+        //memasukkan angka kedua
         System.out.print("Masukkan angka kedua: ");
         double angkaKedua = scanner.nextDouble();
-
+        
+        //memanggil metode pengurangan
         double hasil = angkaPertama - angkaKedua;
+
+        //menampilkan hasil pengurangan
         System.out.println("Hasil pengurangan: " + hasil);
 
         scanner.close();
